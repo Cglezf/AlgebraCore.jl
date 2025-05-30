18d3ab8ffa8053c01431e2ca90420654d29a32e3
Documenter.jl <documenter@juliadocs.github.io>
2025-05-30 03:05:53 +0000

    build based on 9729e72

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AlgebraCore","page":"Home","title":"AlgebraCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AlgebraCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
